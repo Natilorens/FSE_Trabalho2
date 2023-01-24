489bb623819097539df24038b7bd66f0c8e83294
Natilorens <nathalialorenacardoso@gmail.com>
2023-01-23 23:39:33 -0300

    Acrescentando mais funções no protocolo

--- src/uart_Modbus.c
+++ src/uart_Modbus.c
@@ -5,66 +5,101 @@
 #include "../inc/crc16.h"
 #include <string.h>
 #include <wiringPi.h> 
+#include <stdlib.h>
+//#include <Arduino.h>
 
 
 
-int LigaResistor(){
+void enviaSinalControle (){
 
- const int PWM_pin = 4;
- int intensity ;            
+ int uart0_filestream = -1;
 
-  if (wiringPiSetup () == -1)
-    exit (1) ;
-
-  pinMode (PWM_pin, PWM_OUTPUT) ; /* set PWM pin as output */
-
-  while (1)
-  {
-	
-    for (intensity = 0 ; intensity < 100 ; ++intensity)
+    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);      //Open in non blocking read/write mode
+    if (uart0_filestream == -1)
     {
-      pwmWrite (PWM_pin, intensity) ;	/* provide PWM value for duty cycle */
-      delay (1) ;
+        printf("Erro - Não foi possível iniciar a UART.\n");
     }
-    delay(1);
-
-    for (intensity = 100 ; intensity >= 0 ; --intensity)
+    else
     {
-      pwmWrite (PWM_pin, intensity) ;
-      delay (1) ;
+        printf("UART inicializada!\n");
+    }    
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;     //<Set baud rate
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+
+    unsigned char tx_buffer[20];
+    unsigned char *p_tx_buffer;
+    
+    unsigned char mensagemCrc[2];
+
+    p_tx_buffer = &tx_buffer[0];
+    *p_tx_buffer++ = 0x01;
+    *p_tx_buffer++ = 0x16;
+    *p_tx_buffer++ = 0xD1;
+    *p_tx_buffer++ = 6;
+    *p_tx_buffer++ = 9;
+    *p_tx_buffer++ = 0;
+    *p_tx_buffer++ = 9;
+    
+    short valor_Calculado = calcula_CRC(&tx_buffer[0], 7);
+
+    memcpy(mensagemCrc,&valor_Calculado, sizeof(short));
+
+    *p_tx_buffer++ = mensagemCrc[0];
+    *p_tx_buffer++ = mensagemCrc[1];
+
+    printf("Buffers de memória criados!\n");
+    
+    if (uart0_filestream != -1)
+    {
+        printf("Escrevendo caracteres na UART ...");
+        int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
+        if (count < 0)
+        {
+            printf("UART TX error\n");
+        }
+        else
+        {
+            printf("escrito.\n");
+        }
     }
-    delay(1);
-  }
+
+    sleep(1);
+
+    //----- CHECK FOR ANY RX BYTES -----
+    if (uart0_filestream != -1)
+    {
+        // Read up to 255 characters from the port if they are there
+        unsigned char rx_buffer[256];
+        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);      //Filestream, buffer to store in, number of bytes to read (max)
+        if (rx_length < 0)
+        {
+            printf("Erro na leitura.\n"); //An error occured (will occur if there are no bytes)
+        }
+        else if (rx_length == 0)
+        {
+            printf("Nenhum dado disponível.\n"); //No data waiting
+        }
+        else
+        {
+           // float tempInt;
+            //memcpy(&tempInt, &rx_buffer[3], sizeof(float));
+            
+            //Bytes received
+            rx_buffer[rx_length] = '\0';
+            printf("%i Bytes lidos : %.2f\n", rx_length);
+        }
+    }
+
+    close(uart0_filestream);
+
 }
 
-int LigaVentoinha(){
-
- const int PWM_pin = 5;
- int intensity ;            
-
-  if (wiringPiSetup () == -1)
-    exit (1) ;
-
-  pinMode (PWM_pin, PWM_OUTPUT) ; /* set PWM pin as output */
-
-  while (1)
-  {
-	
-    for (intensity = 0 ; intensity < 100 ; ++intensity)
-    {
-      pwmWrite (PWM_pin, intensity) ;	/* provide PWM value for duty cycle */
-      delay (1) ;
-    }
-    delay(1);
-
-    for (intensity = 100 ; intensity >= 0 ; --intensity)
-    {
-      pwmWrite (PWM_pin, intensity) ;
-      delay (1) ;
-    }
-    delay(1);
-  }
-}
 
 void solicitaTemperaturaInterna () {
 
@@ -310,24 +345,43 @@ void leComandosUsuario () {
     {
         // Read up to 255 characters from the port if they are there
         unsigned char rx_buffer[256];
-        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);      //Filestream, buffer to store in, number of bytes to read (max)
-        if (rx_length < 0)
-        {
-            printf("Erro na leitura.\n"); //An error occured (will occur if there are no bytes)
+
+        while(1){
+            int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);      //Filestream, buffer to store in, number of bytes to read (max)
+            if (rx_length < 0)
+            {
+                printf("Erro na leitura.\n"); //An error occured (will occur if there are no bytes)
+            }
+            else if (rx_length == 0)
+            {
+                printf("Nenhum dado disponível.\n"); //No data waiting
+            }
+            else
+            {
+                //float tempInt;
+            // memcpy(&tempInt, &rx_buffer[3], sizeof(float));
+                
+                //Bytes received
+                rx_buffer[rx_length] = '\0';
+                printf("%i Bytes lidos\n", rx_length);
+              switch (rx_buffer[0]) {
+            case 0xA1:
+                // código para a ação correspondente ao comando 0xA1
+                break;
+            case 0xA2:
+                // código para a ação correspondente ao comando 0xA2
+                break;
+            case 0xA3:
+                // código para a ação correspondente ao comando 0xA3
+                break;
+            // adicionar mais casos para os outros comandos
+            default:
+                printf("Comando inválido.\n");
+                break;
         }
-        else if (rx_length == 0)
-        {
-            printf("Nenhum dado disponível.\n"); //No data waiting
-        }
-        else
-        {
-            //float tempInt;
-           // memcpy(&tempInt, &rx_buffer[3], sizeof(float));
-            
-            //Bytes received
-            rx_buffer[rx_length] = '\0';
-            printf("%i Bytes lidos : %.2f\n", rx_length/*, tempInt*/);
-        }
+    }
+
+    usleep(500000); // ler a cada 500 ms
     }
 
     close(uart0_filestream);
@@ -335,8 +389,9 @@ void leComandosUsuario () {
 
 
 
-int main(int argc, const char * argv[]) {
+int main(void) {
 
+     //acendeLed();
     //solicitaTemperaturaInterna();
     //solicitaTemperaturaReferencia();
     //leComandosUsuario();
@@ -347,7 +402,9 @@ int main(int argc, const char * argv[]) {
         printf("1. Solicita Temperatura Interna\n");
         printf("2. Solicita Temperatura de referência\n");
         printf("3. Lê comandos do usuário\n");
-        printf("4. Exit\n");
+        printf("4. Acende LED\n");
+        printf("5. Liga Ventoinha\n");
+        printf("6. Exit\n");
 
         printf("Digite a opcao: ");
         scanf("%d", &opcao);
@@ -363,10 +420,11 @@ int main(int argc, const char * argv[]) {
                 leComandosUsuario();
                 break;
             case 4:
-                LigaResistor();
+                //acendeLed();
+                //LigaResistor();
                 break;
             case 5:
-                LigaVentoinha();
+                //LigaVentoinha();
                 break;
             case 6:
                 printf("Exiting...\n");
@@ -378,5 +436,7 @@ int main(int argc, const char * argv[]) {
         }
     }
 
-   return 0;
+    return 0;
 }
+
+
---
+++ src/ventoinha.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>         
+#include <string.h>
+#include <wiringPi.h> 
+#include <stdlib.h>
+//#include <Wire.h>
+
+
+
+void LigaVentoinha(){
+
+ const int PWM_pin = 5;
+ int intensity ;            
+
+  if (wiringPiSetupGpio () == -1)
+    exit (1) ;
+
+  pinMode (PWM_pin, PWM_OUTPUT) ; // set PWM pin as output
+
+  while (1)
+  {
+	
+    for (intensity = 0 ; intensity < 100 ; ++intensity)
+    {
+      pwmWrite (PWM_pin, intensity) ;	//provide PWM value for duty cycle
+      delay (1) ;
+    }
+    delay(1);
+
+    for (intensity = 100 ; intensity >= 0 ; --intensity)
+    {
+      pwmWrite (PWM_pin, intensity) ;
+      delay (1) ;
+    }
+    delay(1);
+  }
+}
+
+int main (){
+
+    LigaVentoinha();
+
+
+
+    return 0;
+}
